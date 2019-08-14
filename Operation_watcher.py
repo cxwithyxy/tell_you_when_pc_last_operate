ba21d1fdecf32a48ab1ead6a12793d2cfacad96b
chenxuan <523764720@qq.com>
2019-08-14 16:38:33 +0800

    优化监听代码的逻辑

--- Operation_watcher.py
+++ Operation_watcher.py
@@ -1,14 +1,17 @@
 import pyWinhook, pythoncom
 class Operation_watcher:
+    
+    hook_manager:pyWinhook.HookManager
 
-    def OnKeyboardEvent(self, event):
+    def Key_down(self, event):
         print(f'Key:{event.Key}')
         return True
     
+    def __init__(self):
+        self.hook_manager = pyWinhook.HookManager()
+        self.hook_manager.KeyDown = self.Key_down
 
     def watch(self):
-        hm = pyWinhook.HookManager()
-        hm.KeyDown = self.OnKeyboardEvent
-        hm.HookKeyboard()
+        self.hook_manager.HookKeyboard()
         pythoncom.PumpMessages()
         pass
