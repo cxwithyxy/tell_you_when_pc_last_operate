330a1581ad37f2f2f31bd01b529d5464c1cf7483
chenxuan <523764720@qq.com>
2019-08-14 16:54:25 +0800

    现在会存储最后一次操作的名称和时间了

--- Operation_watcher.py
+++ Operation_watcher.py
@@ -1,16 +1,25 @@
-import pyWinhook, pythoncom
+import pyWinhook
+import pythoncom
+import time
+
 class Operation_watcher:
     
     hook_manager: pyWinhook.HookManager
+    last_operate_time: float
+    last_operate_name: str
 
     def user_operate(self, event: pyWinhook.HookEvent):
-        print(f'operate: {event.MessageName}')
+        interval_time = time.time() - self.last_operate_time
+        self.last_operate_time = time.time()
+        self.last_operate_name = event.MessageName
+        # print(f'{interval_time}: {event.MessageName}')
         return True
     
     def __init__(self):
         self.hook_manager = pyWinhook.HookManager()
         self.hook_manager.KeyAll = self.user_operate
         self.hook_manager.MouseAll = self.user_operate
+        self.last_operate_time = time.time()
 
     def watch(self):
         self.hook_manager.HookKeyboard()
